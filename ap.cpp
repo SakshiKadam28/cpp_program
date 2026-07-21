9394bd43127181aae0571c2baf3fff94836aae3b
SakshiKadam28 <kadamsakshi72865@gmail.com>
2026-07-21 09:10:15 +0000

    changes

---
+++ ap.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ float length,breadth;
+ cout<<"Enter the length and breadth :"<<endl;
+ cin>>length>>breadth;
+ cout<<"Perimeter= "<<2*length+2*breadth<<endl;
+ cout<<"Area= "<<length*breadth<<endl;
+ return 0;
+}
+
