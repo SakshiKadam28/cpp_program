7f42cad8666bab7868ab769b16a55229a56393a1
SakshiKadam28 <kadamsakshi72865@gmail.com>
2026-07-21 09:12:52 +0000

    changes

---
+++ ac.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ float radius;
+ const float pi=3.14;
+ cout<<"Enter the radius :"<<endl;
+ cin>>radius;
+ cout<<"Area= "<<pi*radius<<endl;
+ cout<<"Circumference= "<<2*pi*radius<<endl;
+ return 0;
+}
