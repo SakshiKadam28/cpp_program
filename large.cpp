d99dd9a5d98ec80d593a81dca37a23ac02dae746
SakshiKadam28 <kadamsakshi72865@gmail.com>
2026-07-21 09:33:48 +0000

    changes

---
+++ large.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int a,b;
+ cout<<"Enter the value of a and b:"<<endl;
+ cin>>a>>b;
+ if(a>b)
+  cout<<"Largest ="<<a<<endl;
+else
+  cout<<"Largest ="<<b<<endl;
+return 0;
+}
