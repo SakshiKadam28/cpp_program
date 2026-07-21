009ccb13af4511d7d2470fc2abf3c06b26755296
SakshiKadam28 <kadamsakshi72865@gmail.com>
2026-07-21 08:53:57 +0000

    changes

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+ int num1,num2,sum;
+ cout<<"Enter the two numbers:"<<endl;
+ cin>>num1>>num2;
+ sum=num1+num2;
+ cout<<"sum ="<<sum<<endl;
+  return 0;
+} 
+
+
