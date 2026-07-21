55e7f272dfee29942b639b9abf87a02e927ccee5
SakshiKadam28 <kadamsakshi72865@gmail.com>
2026-07-21 09:04:30 +0000

    changes

---
+++ op.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int a,b;
+ cout<<"Enter the two numbers:"<<endl;
+ cin>>a>>b;
+ cout<<"Addition ="<<a+b<<endl;
+ cout<<"Subtraction ="<<a-b<<endl;
+ cout<<"Multiplication ="<<a*b<<endl;
+ cout<<"Divion ="<<a/b<<endl;
+ cout<<"Modulus ="<<a%b<<endl;
+ return 0;
+}
