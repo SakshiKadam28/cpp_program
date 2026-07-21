23449280bef32acf33721a63aa13127ae6bd4226
SakshiKadam28 <kadamsakshi72865@gmail.com>
2026-07-21 09:55:15 +0000

    change

---
+++ pn.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int num;
+ cout<<"Enter the number:"<<endl;
+ cin>>num;
+if(num>0)
+ cout<<"Number is Positive"<<endl;
+else if(num<0)
+  cout<<"Number is Negarive"<<endl;
+else
+  cout<<"Zero"<<endl;
+return 0;
+}
