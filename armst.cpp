0a96ffa9d4cb506f2646c5b7ec4cc6fa59f33058
SakshiKadam28 <kadamsakshi72865@gmail.com>
2026-08-03 16:08:48 +0000

    changes

---
+++ armst.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int n,temp, rem, sum = 0, digits=0;
+    cout<<"Enter a number:";
+    cin>>n;
+    temp =n;
+    while(temp!=0)
+    {
+        digits++;
+        temp/=10;
+    }
+        temp = n;
+
+    while (temp != 0) {
+        rem = temp % 10;
+        sum += pow(rem, digits);
+        temp /= 10;
+    }
+
+    if (sum == n)
+        cout << "Armstrong Number";
+    else
+        cout << "Not an Armstrong Number";
+    return 0;
+
+}
